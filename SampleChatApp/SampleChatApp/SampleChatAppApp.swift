163e22a2845202e41a9beea13f0644c81426d07e
dineswarareddy <dineswarareddy@gmail.com>
2024-11-07 22:51:42 +0530

    Created project. initial commit

---
+++ SampleChatApp/SampleChatApp/SampleChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleChatAppApp.swift
+//  SampleChatApp
+//
+//  Created by Dineswarareddy on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleChatApp/SampleChatAppTests/SampleChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleChatAppTests.swift
+//  SampleChatAppTests
+//
+//  Created by Dineswarareddy on 07/11/24.
+//
+
+import Testing
+@testable import SampleChatApp
+
+struct SampleChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
