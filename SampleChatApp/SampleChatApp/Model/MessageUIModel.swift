59b6ffa048e08c368820017033efda376cef17a1
dineswarareddy <dineswarareddy@gmail.com>
2024-11-08 22:15:47 +0530

    Renamed MessageModel to MessageUIModel.

--- SampleChatApp/SampleChatApp/Model/MessageUIModel.swift
+++ SampleChatApp/SampleChatApp/Model/MessageUIModel.swift
@@ -1,5 +1,5 @@
 //
-//  MessageModel.swift
+//  MessageUIModel.swift
 //  SampleChatApp
 //
 //  Created by Dineswarareddy on 07/11/24.
@@ -12,13 +12,23 @@ enum SourceOfMessage {
   case fromChatbot
 }
 
-struct MessageModel: Identifiable, Hashable {
+enum MessageType: Hashable {
+  case text
+  case image(imageUrl: String)
+  case video(videoUrl: String)
+}
+
+struct MessageUIModel: Identifiable, Hashable, Equatable {
   internal init(message: String,
-                sourceOfTheMessage: SourceOfMessage) {
+                sourceOfTheMessage: SourceOfMessage,
+                type: MessageType = .text) {
     self.message = message
     self.sourceOfTheMessage = sourceOfTheMessage
+    self.messageType = type
   }
+
   let id: String = UUID().uuidString
   let message: String
   let sourceOfTheMessage: SourceOfMessage
+  let messageType: MessageType
 }
