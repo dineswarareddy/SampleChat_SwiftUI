2d9af70c870f89d4f6d63e1027fd729de3091bfe
dineswarareddy <dineswarareddy@gmail.com>
2024-11-08 10:04:38 +0530

    created message views for human message and chatbot message

---
+++ SampleChatApp/SampleChatApp/Views/MessageView.swift
@@ -0,0 +1,68 @@
+//
+//  MessageView.swift
+//  SampleChatApp
+//
+//  Created by Dineswarareddy on 07/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MessageView: View {
+  var messageModel: MessageModel
+  var body: some View {
+    if messageModel.sourceOfTheMessage == .fromChatbot {
+      ChatbotMessageView(message: messageModel.message)
+    } else {
+      HumanMessageView(message: messageModel.message)
+    }
+  }
+}
+
+fileprivate struct ChatbotMessageView: View {
+  var message: String
+  var body: some View {
+    HStack {
+      Text(message)
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+        .background(Color(UIColor.chatBackgroundGray()))
+        .foregroundStyle(.black)
+        .clipShape(
+            .rect(
+                topLeadingRadius: 20,
+                bottomLeadingRadius: 0,
+                bottomTrailingRadius: 20,
+                topTrailingRadius: 20
+            )
+        )
+      Spacer()
+    }
+  }
+}
+
+fileprivate struct HumanMessageView: View {
+  var message: String
+  var body: some View {
+    HStack {
+      Spacer()
+      Text(message)
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+        .background(Color(UIColor.chatBlueColor()))
+        .foregroundStyle(.white)
+        .clipShape(
+            .rect(
+                topLeadingRadius: 20,
+                bottomLeadingRadius: 20,
+                bottomTrailingRadius: 0,
+                topTrailingRadius: 20
+            )
+        )
+    }
+  }
+}
+
+#Preview {
+  MessageView(messageModel: MessageModel(message: "Message 1", sourceOfTheMessage: .fromChatbot))
+}
